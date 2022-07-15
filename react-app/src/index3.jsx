2e9ce9feeb5e95431adf19953daed8b54b36a5a8
paramita <baqkoo007@aliyun.com>
2022-07-15 17:37:19 +0800

    add react app

---
+++ react-app/src/index3.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const name = "Josh Perez";
+const element = <h1>hello {name}</h1>;
+
+ReactDOM.render(element, document.getElementById("root"));
